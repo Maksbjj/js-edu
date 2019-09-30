43845ec42a74bb2e0eb72f6c509e08bec5510090
Maxim <maxim3093@gmail.com>
2019-09-30 17:22:17 +0200

    init: add solution

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+module.exports = function getTimeForEducation(focus, knowsProgramming, config) {
+  let masterTime = 800;
+  let basicTime = 500;
+  let weeks = 0;
+  if (knowsProgramming == true) {
+    weeks += Math.ceil(masterTime / config[focus]);
+  } else {
+    weeks += Math.ceil((masterTime + basicTime) / config[focus]);
+  }
+  return weeks;
+};
